ad57baf2856cc4671caf86cbcde88da68313a274
shahilvk <shahilvk99@gmail.com>
2025-10-04 09:51:49 +0530

    Redux-Task1

---
+++ react-redux/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Counter from './component/Counter'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Redux counter example</h1>
+      <Counter />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ react-redux/src/component/CounterReducer.jsx
@@ -0,0 +1,17 @@
+
+const CounterReducer = (state = {count:0}, action)=>{
+    switch(action.type){
+        case "INCREMENT" :
+            return{count : state.count+1};
+        case "DECREMENT" :
+            return{count : state.count-1};
+        default :
+            return state;        
+    }
+
+}
+
+export const increment = ()=>({type:"INCREMENT"});
+export const decrement = ()=> ({type:"DECREMENT"});
+
+export default CounterReducer;
